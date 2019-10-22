958b1658e4ffdcb56a2355d9bb9d2ef97087ceef
John Flournoy <uoflournoy@gmail.com>
2019-10-22 15:50:23 -0400

    updated

--- R/gen_syntax.R
+++ R/gen_syntax.R
@@ -1,204 +1,277 @@
-#' make_riclpm_lavaan
+#' a_random_intercept
 #'
-#' @param xname name of x variable
-#' @param yname name of y variable
-#' @param waveints integers specifying wave labels
-#' @param constrain_all set all constraints via labels
-#' @param alpha set label or value for alpha constraint
-#' @param delta set label or value for delta constraint
-#' @param beta set label or value for beta constraint
-#' @param gamma set label or value for gamma constraint
-#' @param uu set label or value for uu constraint
-#' @param vv set label or value for vv constraint
-#' @param uv set label or value for uv constraint
-#' @param kk set label or value for kk constraint
-#' @param oo set label or value for oo constraint
-#' @param ko set label or value for ko constraint
-#' @param pp set label or value for pp constraint
-#' @param qq set label or value for qq constraint
-#' @param pq set label or value for pq constraint
+#' Create syntax for a single random intercept term based on a vector of
+#' variable names and a variable name for the random intercept term. Used
+#' internally by \code{\link{random_intercepts}}.
 #'
-#' @return A character string that is syntax for an RI-CLPM model as specified
-#' @export
+#' @param vars Vector of variable names (column names in the target data.frame).
+#' @param varname Used to name the latent random intercept.
 #'
-make_riclpm_lavaan <- function(xname = 'x', yname = 'y', waveints, slope = F, slope_center = 1, constrain_all = F,
-                               int.lv.free = F,
-                               alpha = '', delta = '', beta = '', gamma = '', uu = '', vv = '', uv = '', pp = '', qq = '', pq = '',
-                               kk = '', oo = '', ksks = '', osos = '', kks = '', oos = '', ko = '', kos = '', ksos = '', kso = ''){
-
-  waveids <- waveints
-  if(constrain_all){
-    b <- list(alpha = 'alpha',
-              delta = 'delta',
-              beta = 'beta',
-              gamma = 'gamma',
-              uu = 'uu',
-              vv = 'vv',
-              uv = 'uv',
-              kk = 'kk',
-              oo = 'oo',
-              ksks = 'ksks',
-              osos = 'osos',
-              kks = 'kks',
-              oos = 'oos',
-              ko = 'ko',
-              kos = 'kos',
-              ksos = 'ksos',
-              kso = 'kso',
-              pp = 'pp',
-              qq = 'qq',
-              pq = 'pq')
-  }
-  b_ <- list(alpha = alpha,
-             delta = delta,
-             beta = beta,
-             gamma = gamma,
-             uu = uu,
-             vv = vv,
-             uv = uv,
-             kk = kk,
-             oo = oo,
-             ksks = ksks,
-             osos = osos,
-             kks = kks,
-             oos = oos,
-             ko = ko,
-             kos = kos,
-             ksos = ksos,
-             kso = kso,
-             pp = pp,
-             qq = qq,
-             pq = pq)
-
-  for(b_i in 1:length(b)) {
-    if(b_[[b_i]] != ''){
-      b[[b_i]] <- b_[[b_i]]
-    }
-    if(b[[b_i]] != ''){
-      b[[b_i]] = paste0(b[[b_i]], '*')
-    }
-  }
-
-  latent_means <- paste(paste0(c('kappa =~ ', 'omega =~ '),
-                               lapply(lapply(c(xname, yname), paste0, waveids),
-                                      function(vars){
-                                        paste(paste0('1*', vars), collapse = ' + ')
-                                      })),
-                        collapse = '\n')
-
-  if(int.lv.free){
-    intercepts <- paste(unlist(lapply(list(c(xname,'0'), c(yname, '0')),
-                                      function(pair){
-                                        lapply(waveids, function(wave){
-                                          paste0(pair[1], wave, ' ~ ', pair[2], '*1')
-                                        })
-                                      })), collapse = '\n')
-    latent_intercepts <- 'kappa ~ k_m*1
-omega ~ o_m*1'
-    if(slope){
-      latent_intercepts <- paste(latent_intercepts,
-'kappa_s ~ ks_m*1
-omega_s ~ os_m*1', sep = '\n')
-    }
-    intercepts <- paste(intercepts, latent_intercepts, sep = '\n')
-  } else {
-    intercepts <- paste(unlist(lapply(list(c(xname,'mu'), c(yname, 'pi')),
-                                      function(pair){
-                                        lapply(waveids, function(wave){
-                                          paste0(pair[1], wave, ' ~ ', pair[2], wave, '*1')
-                                        })
-                                      })), collapse = '\n')
-    if(slope){
-      intercepts <- paste(intercepts,
-'kappa_s ~ ks_m*1
-omega_s ~ os_m*1', sep = '\n')
-    }
-  }
-
-  if(slope){
-    latent_slopes <- paste(paste0(c('kappa_s =~ ', 'omega_s =~ '),
-                                  lapply(lapply(c(xname, yname), paste0, waveids),
-                                         function(vars){
-                                           paste(paste0(waveids - slope_center, '*', vars), collapse = ' + ')
-                                         })),
-                           collapse = '\n')
-    latent_means <- paste(latent_means, latent_slopes, sep = '\n')
-    latent_covar <- paste0('
-                         kappa ~~ ',b$kk,'kappa #variance
-                         omega ~~ ',b$oo,'omega #variance
-                         kappa_s ~~ ',b$ksks,'kappa_s #variance
-                         omega_s ~~ ',b$osos,'omega_s #variance
-                         kappa ~~ ',b$kks,'kappa_s #covariance
-                         omega ~~ ',b$oos,'omega_s #covariance
-                         kappa ~~ ',b$ko,'omega #covariance
-                         kappa ~~ ',b$kos,'omega_s #covariance
-                         kappa_s ~~ ',b$kso,'omega #covariance
-                         kappa_s ~~ ',b$ksos,'omega_s #covariance')
-  } else {
-    latent_covar <- paste0('
-                         kappa ~~ ',b$kk,'kappa #variance
-                         omega ~~ ',b$oo,'omega #variance
-                         kappa ~~ ',b$ko,'omega #covariance')
-  }
-
-  latent_resids <- paste(unlist(lapply(list(c('p',xname), c('q', yname)),
-                                       function(pair){
-                                         lapply(waveids, function(wave){
-                                           paste0(pair[1], wave, ' =~ ', '1*', pair[2], wave)
-                                         })
-                                       })), collapse = '\n')
-
-  regressions <- paste(
-    unlist(lapply(list(c('p','q', b$alpha, b$beta), c('q', 'p', b$delta, b$gamma)),
-                  function(pair){
-                    lapply(rev(waveids[-1]), function(wave){
-                      paste0(pair[1], wave, ' ~ ', pair[3], pair[1], wave-1, ' + ', pair[4], pair[2], wave-1)
-                    })
-                  })), collapse = '\n')
-
-  first_wave_varcovar <- paste0('
-p', waveids[1], ' ~~ ', b$pp,'p', waveids[1], '
-q', waveids[1], ' ~~ ', b$qq,'q', waveids[1], '
-p', waveids[1], ' ~~ ', b$pq,'q', waveids[1])
-
-  residvar <- paste(unlist(lapply(list(c('p', b$uu), c('q', b$vv)),
-                                  function(avar){
-                                    lapply(waveids[-1], function(wave){
-                                      paste0(avar[1], wave, ' ~~ ', avar[2], avar[1], wave)
-                                    })
-                                  })), collapse = '\n')
-
-  contemporaneous <- paste(lapply(waveids[-1],
-                                  function(wave){
-                                    paste0('p', wave, ' ~~ ', b$uv, 'q', wave)
-                                  }),
-                           collapse = '\n')
-
-  constraints <- '
-kk > 0.00001
-oo > 0.00001'
-  if(slope){
-    constraints <- paste(constraints,
-'ksks > 0
-osos > 0', sep = '\n')
-  }
-
-  generatingModel <- paste(latent_means,
-                           intercepts,
-                           latent_covar,
-                           latent_resids,
-                           regressions,
-                           first_wave_varcovar,
-                           residvar,
-                           contemporaneous,
-                           constraints, sep = '\n')
-  return(generatingModel)
+#' @return A list with the model text in \code{model_text}, random intercept
+#'   name in \code{varname}, and \code{type} set to 'latent' (not used yet).
+#'
+#' @examples
+a_random_intercept <- function(vars, varname){
+  ri_name <- paste0('ri_', varname)
+  right_side <- paste(paste0('1*', vars), collapse = ' + ')
+  ri_text <- paste0(ri_name, ' =~ ', right_side)
+  return(list(model_text = ri_text, varname = ri_name, type = 'latent'))
 }
 
+#' random_intercepts
+#'
+#' Creates random intercept model text for all sets of variables passed via
+#' \code{var_groups}.
+#'
+#' @param var_groups A list of vectors of variable names. Each element of the
+#'   list will be used to generate model text for a latent random intercept.
+#'
+#' @return A matrix with columns for each top-level element of \code{var_groups}
+#'   and rows for each element of the list returned by
+#'   \code{\link{a_random_intercept}}.
+#'
+#' @examples
+random_intercepts <- function(var_groups){
+  return(mapply(a_random_intercept,
+                vars = var_groups,
+                varname = names(var_groups)))
+}
+
+#' simple_vars
+#'
+#' Creates model text that covaries each element of \code{terms1} with each
+#' element of \{terms2}.
+#'
+#' @param terms1 A vector of variable names.
+#' @param terms2 A vector of variable names.
+#' @param constrain Set all variances to equality. If not \code{FALSE}, provide
+#'   the name for the equality constraint.
+#'
+#' @return Model text specifying covariances between terms.
+#' @export
+#'
+simple_vars <- function(terms1, terms2, constrain = NULL){
+  if(!is.null(constrain)){
+    terms2 <- paste0(constrain, '*', terms2)
+  }
+  vars <- paste(paste0(terms1, ' ~~ ', terms2), collapse = '\n')
+  return(vars)
+}
+
+#' residual_variances
+#'
+#' Creat model text specifying residual variances, possibly with constraints to
+#' eqaulity across waves.
+#'
+#' @param var_groups A list of vectors of variable names. Each element of the
+#'   list will be used to generate model text for residual variances, possibly
+#'   with equality constraints across waves.
+#'
+#' @return
+#' @export
+#'
+#' @examples
+residual_variances <- function(var_groups, constrain = TRUE){
+  residual_text <- mapply(function(vars, varname){
+    if(constrain){
+      first_res_vars <- simple_vars(vars[1], vars[1], constrain = NULL)
+      const_res_vars <- simple_vars(vars[-1], vars[-1], constrain = paste0('e_', varname))
+      res_vars <- paste(c(first_res_vars, const_res_vars), collapse = '\n')
+    } else {
+      res_vars <- simple_vars(vars, vars)
+    }
+    return(res_vars)
+  }, var_groups, names(var_groups))
+
+  return(paste(unlist(residual_text), collapse = '\n'))
+}
+
+#' varcovars
+#'
+#' Creates model text that specifies variances for each variable in
+#' \code{terms}, as well as the full covariance matrix between those terms.
+#'
+#' @param terms Vector of variable names to create variance-covariance
+#'   specification from.
+#'
+#' @return Model text specifying covariance between terms in \code{terms}.
+#' @export
+#'
+varcovars <- function(terms){
+  vars <- simple_vars(terms, terms)
+  nterms <- length(terms)
+  covars <- lapply(1:(nterms-1), function(i){
+    right_side <- paste(terms[(i+1):nterms], collapse = ' + ')
+    return(paste0(terms[i], ' ~~ ', right_side))
+  })
+  return(paste(c(vars, covars), collapse = '\n'))
+}
+
+#' manifest_ints
+#'
+#' Create model text that specifies intercepts for each term.
+#'
+#' @param terms Vector of variable names to create intercept specifications for.
+#' @param constraint Whether to constrain the intercepts to a certain value. \code{'free'} is the default. Provide the specific value for the contraint.
+#'
+#' @return Model text for specifying intercepts
+#' @export
+#'
+manifest_ints <- function(terms, constrain = 'free'){
+  if(constrain != 'free'){
+    mus <- paste0(constrain, '*1')
+  } else {
+    mus <- paste0(terms, '_mu*1')
+  }
+  ints <- paste0(terms, ' ~ ', mus)
+  return(paste(ints, collapse = '\n'))
+}
+
+#' a_latent_resid_set
+#'
+#' Creates latent residual terms for each variable in \code{vars}, using
+#' \code{varname} to name them.
+#'
+#' @param vars Vector of variable names (column names in the target data.frame).
+#' @param varname Used to name the latent residuals.
+#'
+#' @return A list with the model text in \code{model_text}, latent residual
+#'   names in \code{varname}, and \code{type} set to 'latent' (not used yet).
+#' @export
+#'
+a_latent_resid_set <- function(vars, varname){
+  nvars <- length(vars)
+  right_side <- paste0('1*', vars)
+  latent_names <- paste0('lat_', varname, 1:nvars)
+  latent_resids <- paste0(latent_names, ' =~ ', right_side)
+  return(list(model_text = latent_resids,
+              varname = latent_names,
+              type = 'latent'))
+
+}
+
+#' latent_resids
+#'
+#' Creates latent residuals model text for all sets of variables passed via
+#' \code{var_groups}.
+#'
+#' @param var_groups A list of vectors of variable names. Each element of the
+#'   list will be used to generate model text for a set of latent residuals
+#'   based on the variables in that element.
+#'
+#' @return A matrix with columns for each top-level element of \code{var_groups}
+#'   and rows for each element of the list returned by
+#'   \code{\link{a_latent_resid_set}}.
+#' @export
+#'
+#' @examples
+#' some_vars <- list(x = c('x1', 'x2'), y = c('y1', 'y2'))
+#' latent_resids(some_vars)
+latent_resids <- function(var_groups){
+  return(mapply(a_latent_resid_set,
+                vars = var_groups,
+                varname = names(var_groups)))
+}
+
+#' clpm
+#'
+#' Create model text specifying the cross-lag panel model portion of the
+#' RI-CLPM, including all autoregressive and cross-lag effects. Set
+#' \code{constrain} to \code{TRUE} to constrain these to be equal across all
+#' waves.
+#'
+#' @param lat_resid_vars A list of vectors of variable names of the latent
+#'   residual variables. Each element should be a vector of variable names that
+#'   corresponds to the same construct measured repeatedly over waves.
+#' @param constrain Constrain the path coefficiecients to be the same from
+#'   wave-to-wave?
+#'
+#' @return Model text specifying the CLPM portion of the model.
+#' @export
+#'
+clpm <- function(lat_resid_vars, constrain = TRUE){
+  adf <- as.data.frame(lat_resid_vars)
+  nsets <- dim(adf)[2]
+  nvars <- dim(adf)[1]
+  set_names <- names(adf)
+  cross_lags <- lapply(1:nsets, function(i){
+    left_side <- adf[-1, i]
+    left_var <- set_names[i]
+    if(constrain){
+      right_side_adf <- data.frame(mapply( function(col, aname){
+        constraint_name <- paste0(left_var, '_', aname)
+        paste0(constraint_name, '*', col)
+      }, adf[-nvars,], set_names))
+    } else {
+      right_side_adf <- adf
+    }
+    right_side <- apply(right_side_adf, 1, paste, collapse = ' + ')
+    paste0(left_side, ' ~ ', right_side)
+  })
+  contemps <- contemp_covars(lat_resid_vars, constrain = constrain)
+  return(paste(c(contemps, unlist(cross_lags)), collapse = '\n'))
+}
+
+#' contemp_covars
+#'
+#' Create model text to specify contemporaneous covariances between latent
+#' residuals.
+#'
+#' @param lat_resid_vars A list of vectors of latent residual variable names.
+#' @param constrain Constrain the covariances to be the same across waves?
+#'
+#' @return Model text specifying the contemporaneous covariances.
+#' @export
+#'
+contemp_covars <- function(lat_resid_vars, constrain = TRUE){
+  nsets <- length(lat_resid_vars)
+  covars <- lapply(1:(nsets-1), function(i){
+    left_side <- lat_resid_vars[[i]]
+    if(constrain){
+      lname <- names(lat_resid_vars)[[i]]
+      rnames <- names(lat_resid_vars)[(i+1):nsets]
+      constraint_names <- paste0('r_', lname, rnames)
+      lat_resid_df <- data.frame(lat_resid_vars[(i+1):nsets], stringsAsFactors = FALSE)
+      lat_resid_df_top_row <- lat_resid_df[1,]
+      lat_resid_df_the_rest <- lat_resid_df[-1,]
+      right_side_adf <- data.frame(mapply( function(col, cname){
+        paste0(cname, '*', col)
+      }, lat_resid_df_the_rest, constraint_names), stringsAsFactors = FALSE)
+      right_side_adf <- rbind(lat_resid_df_top_row, right_side_adf)
+    } else {
+      right_side_adf <- data.frame(lat_resid_vars[(i+1):nsets])
+    }
+    right_side <- apply(right_side_adf, 1, paste, collapse = ' + ')
+    return(paste0(left_side, ' ~~ ', right_side))
+  })
+  return(paste(unlist(covars), collapse = '\n'))
+}
+
+riclpm_text <- function(var_groups, constrain_over_waves = TRUE, constrain_ints = 'free'){
+  ri_terms <- random_intercepts(var_groups = var_groups)
+  ri_text <- paste(ri_terms['model_text',], collapse = '\n')
+  ri_varcov_text <- varcovars(ri_terms['varname',])
+  man_ints_text <- manifest_ints(unlist(var_groups), constrain = constrain_ints)
+  lat_resid_terms <- latent_resids(var_groups = var_groups)
+  lat_resid_text <- paste0(unlist(lat_resid_terms['model_text',]), collapse = '\n')
+  lat_resid_vars <- lat_resid_terms['varname',]
+  clpm_text <- clpm(lat_resid_vars, constrain = constrain_over_waves)
+  resid_vars_text <- residual_variances(lat_resid_vars, constrain = constrain_over_waves)
+
+  lavmod <- paste(c(ri_text,
+                    ri_varcov_text,
+                    man_ints_text,
+                    lat_resid_text,
+                    clpm_text,
+                    resid_vars_text),
+                  collapse = '\n')
+  return(lavmod)
+}
 
 #' lavriclpm
 #'
-#' This passes through a model and data to the lavaan::lavaan
+#' This passes through a model and data to the lavaan::lavaan function with
+#' proper constraints.
 #'
 #' @param riclpmModel The lavaan syntax for the RI-CLPM model
 #' @param data The data
